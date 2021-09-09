62cc6caf02d767318990df4781164ebacf0970c3
waltersotil <66787559+waltersotil@users.noreply.github.com>
2021-09-09 12:25:24 -0300

    Update Main.gd
    Se edito main.gd

--- Main.gd
+++ Main.gd
@@ -20,7 +20,7 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
-	$HUD.show_message("Get Ready!")
+	$HUD.show_message("Get Ready!!!")
 	$Music.play()
 
 
